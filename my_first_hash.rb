7cdde8f259060e70de7b700084a99e6b04803226
Toni  Diep <adiep123@gmail.com>
2020-06-04 06:12:58 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,7 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+ client = {"name" => "Maria", "age" => 30, "birthplace" => "Nevada"}
+end
 
   
 end
